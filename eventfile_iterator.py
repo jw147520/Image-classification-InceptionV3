d4b632944596516865d7234cda3381d8b2bacd4b
jw2913 <jw2913@users.noreply.github.com>
2017-10-12 14:15:16 +0900

    added a directory containing retrain.py

--- eventfile_iterator.py
+++ eventfile_iterator.py
@@ -1,18 +1,24 @@
+# -*- coding: utf-8 -*-
+# tensorboard log file 을 사용해 보다 높은 resolution 의 그래프를 그린다.
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-path_to_train_events_file = './log/train/events.out.tfevents.1501641886.MAZELONESERVER'
-path_to_validation_events_file = './log/validation/events.out.tfevents.1501641892.MAZELONESERVER'
+# 아래 경로에서 [put event_file_name here] 를 실제 event file 명으로 바꿔야 함.
+# ex) path_to_train_events_file = "./log/train/event.out.tfevents ... "
+path_to_train_events_file = './log/train/[put event_file_name here]'
+path_to_validation_events_file = './log/validation/[put event_file_name here]'
 
+# log file 을 iterate 하며 training step 당 각각의 accuracy/cross-entropy 값을 저장할 lists.
 training_accuracy = []
 validation_accuracy = []
 training_CE = []
 validation_CE = []
 
-step = 0
-CE_step = 0
-training_x = []
-training_CE_x = []
+step = 0  # training step - accuracy
+CE_step = 0  # training step - cross entropy
+# step = CE_step 이지만 구분하여 사용하였음.
+training_x = []  # training-accuracy 그래프를 그리기 위한 x 축 값 list : training step
+training_CE_x = []  # training-CrossEntropy 그래프를 그리기 위한 x 축 값 list : training step
 for e in tf.train.summary_iterator(path_to_train_events_file):
     for v in e.summary.value:
         if v.tag == 'accuracy_1':
@@ -26,10 +32,11 @@ for e in tf.train.summary_iterator(path_to_train_events_file):
             # print(v.simple_value)
             CE_step += 1
 
-step = 0
-CE_step = 0
-validation_x = []
-validation_CE_x = []
+step = 0  # training step - accuracy
+CE_step = 0  # training step - cross entropy
+# step = CE_step 이지만 구분하여 사용하였음.
+validation_x = []  # validation-accuracy 그래프를 그리기 위한 x 축 값 list : training step
+validation_CE_x = []  # validation-CrossEntropy 그래프를 그리기 위한 x 축 값 list : training step
 for e in tf.train.summary_iterator(path_to_validation_events_file):
     for v in e.summary.value:
         if v.tag == 'accuracy_1':
